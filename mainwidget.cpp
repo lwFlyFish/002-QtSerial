69e85d7cd211f5494e9468a3d362a2c54cf6f36f
lwFlyFIsh <lw_liuwei@126.com>
2020-03-31 22:30:12 +0800

    创建数据库

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -5,6 +5,7 @@
 #include <QSqlDatabase>
 #include <QSqlError>
 #include <QSqlQuery>
+#include <QMessageBox>
 
 MainWidget::MainWidget(QWidget *parent):
     QWidget(parent),
@@ -24,18 +25,39 @@ void MainWidget::serial_start_init()
     this->setWindowTitle(QString("Serial"));
 
     // 判断是否创建数据库
+    QSqlDatabase datebase_serial_parameter=QSqlDatabase::addDatabase("QSQLITE");
+    datebase_serial_parameter.setDatabaseName(QApplication::applicationDirPath()+"/datebase.db");
+    if (!datebase_serial_parameter.open()) {
+           QMessageBox::warning(0, QObject::tr("Database Error"),datebase_serial_parameter.lastError().text());
+    }
+
     QSqlQuery query;
-    bool isTableExist =query.exec(QString("select count(*) from sqlite_master where type='table' and name='%1'").arg("CANexcel"));
-    if(false==isTableExist)
-    {
-        qDebug() << "table does not exist";
-    }
-    else
-    {
-        //QMessageBox::critical(new QWidget,QString("创建数据库"),QString("数据库已经存在"));
-        qDebug() << "table exists";
-    }
-        //return QSqlError();
+    //创建表
+    query.exec("CREATE TABLE seria_parameter("
+               "name VARCHAR(20) NOT NULL,"
+               "data INTEGER NOT NULL)");
+    //导入数据
+    query.exec("INSERT INTO serial_parameter (name,data)"
+               "VALUES ('port','3')");
+    query.exec("INSERT INTO serial_parameter (name,data)"
+               "VALUES ('baudrate','9600')");
+    query.exec("INSERT INTO serial_parameter (name,data)"
+               "VALUES ('StopBit','1')");
+    query.exec("INSERT INTO serial_parameter (name,data)"
+               "VALUES ('DataBit','8')");
+    query.exec("INSERT INTO serial_parameter (name,data)"
+               "VALUES ('check','0')");
+
+    query.exec("CREATE TABLE students ("
+                       "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                       "name VARCHAR(40) NOT NULL, "
+                       " score INTEGER NOT NULL, "
+                       "class VARCHAR(40) NOT NULL)");
+    query.exec("INSERT INTO students (name, score,class) "
+                   "VALUES ('小张', 85, '初2-1班')");
+                  //向students表里的(name, score,class)标题下插入一项数据'小张', 85, '初2-1班'
+
+
 
 
 
